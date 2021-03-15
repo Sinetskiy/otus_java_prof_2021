3a76347c8ec3870dcc8143cc887b209601b460c5
Andrey Sinetskiy <sinetskiy@gmail.com>
2021-03-15 23:24:41 +0300

    hw8 - first solution

--- HW08-serialization/src/main/java/ru/otus/dataprocessor/MeasurementDeserializer.java
+++ HW08-serialization/src/main/java/ru/otus/dataprocessor/MeasurementDeserializer.java
@@ -1,13 +1,9 @@
 package ru.otus.dataprocessor;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.node.DoubleNode;
-import com.fasterxml.jackson.databind.node.TextNode;
-import org.gradle.internal.impldep.com.fasterxml.jackson.databind.node.IntNode;
 import ru.otus.model.Measurement;
 
 import java.io.IOException;
