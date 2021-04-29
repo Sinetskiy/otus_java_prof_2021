14683777886dd73ddc96d2664cc5360e74ab5bf8
Andrey Sinetskiy <sinetskiy@gmail.com>
2021-04-29 22:35:30 +0300

    hw25 - DI changes

--- HW25-DI/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ HW25-DI/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -13,58 +13,56 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
-    public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+    public AppComponentsContainerImpl(Class<?> initialConfigClass) throws Exception {
         processConfig(initialConfigClass);
     }
 
-    private void processConfig(Class<?> configClass) {
-        checkConfigClass(configClass);
-        Constructor<?> ctor = null;
-        try {
-            ctor = configClass.getConstructor();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-        try {
-            Object appConfig = ctor.newInstance();
-            var methods = configClass.getMethods();
-            for (var i = 0; i < 3; i++) {
-                for (Method method : methods) {
-                    if (method.isAnnotationPresent(AppComponent.class)) {
-                        AppComponent annotations = method.getAnnotation(AppComponent.class);
-                        if (annotations.order() == i) {
-                            var params = method.getParameterTypes();
-                            ArrayList<Object> args = new ArrayList<>();
-                            for (var param : params) {
-                                args.add(appComponentsByName.get(param.getTypeName()));
-                            }
-                            Object object;
-                            if (args.size() == 0) {
-                                object = method.invoke(appConfig);
-                                collectComponents(method, annotations, object);
-                            }
-                            if (args.size() == 1) {
-                                object = method.invoke(appConfig, args.get(0));
-                                collectComponents(method, annotations, object);
-                            }
-                            if (args.size() == 3) {
-                                object = method.invoke(appConfig, args.get(0), args.get(1), args.get(2));
-                                collectComponents(method, annotations, object);
-                            }
-                        }
-                    }
-                }
-            }
-
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
+    private void processConfig(Class<?> configClass) throws Exception {
+        Constructor<?> ctor = getConstructor(configClass);
+        setCollectionOfComponents(configClass, ctor);
     }
 
-    private void collectComponents(Method method, AppComponent annotations, Object object) {
-        appComponentsByName.put(method.getReturnType().getTypeName(), object);
-        appComponentsByName.put(annotations.name(), object);
+    private Constructor<?> getConstructor(Class<?> configClass) throws Exception {
+        checkConfigClass(configClass);
+        Constructor<?> ctor;
+        ctor = configClass.getConstructor();
+        return ctor;
+    }
+
+    private void setCollectionOfComponents(Class<?> configClass, Constructor<?> ctor) throws Exception {
+            Object appConfig = ctor.newInstance();
+            var methods = configClass.getMethods();
+            for (var i = 0; i < methods.length; i++) { // но вообще говоря ордер может быть и 100 200 300 ...
+                for (Method method : methods) {
+                    addComponent(appConfig, i, method);
+                }
+            }
+    }
+
+    private void addComponent(Object appConfig, int i, Method method) throws IllegalAccessException, InvocationTargetException {
+        if (method.isAnnotationPresent(AppComponent.class)) {
+            AppComponent annotations = method.getAnnotation(AppComponent.class);
+            if (annotations.order() == i) {
+                var params = method.getParameterTypes();
+                List<Object> args = new ArrayList<>();
+                for (var param : params) {
+                    args.add(appComponentsByName.get(param.getTypeName()));
+                }
+//                System.out.println(method.getName());
+//                System.out.println(appConfig.toString());
+//                for (var arg : args) {
+//                    System.out.println(arg);
+//                }
+//                var object =  method.invoke(appConfig, args); // <- запустите потом расскажете как этот метод работает
+                var object = args.size() != 0 ?
+                                            args.size() != 1 ?
+                                                    method.invoke(appConfig, args.get(0), args.get(1), args.get(2))
+                                                    : method.invoke(appConfig, args.get(0))
+                                                            : method.invoke(appConfig);
+                appComponentsByName.put(method.getReturnType().getTypeName(), object);
+                appComponentsByName.put(annotations.name(), object);
+            }
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
